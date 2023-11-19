64f096a729a6f9a3096c69115d513482be23aaf9
Dragomir Balinov <dragomir.balinov@idrettsmail.no>
2023-11-19 20:40:21 +0100

    feat: add videojs player with youtube plugin

---
+++ TubeLess/wwwroot/js/video-js/player.js
@@ -0,0 +1,11 @@
+﻿function loadPlayer(id, options) {
+    disposePlayer(id);
+    videojs(id, options);
+}
+
+function disposePlayer(id) {
+    const player = videojs.getPlayer(id);
+    if (player) {
+        player.dispose();
+    }
+}
