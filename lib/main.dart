baf03f4689d436b792dcd42de5e0f9fd9034584f
Ventsislav Georgiev <ventsislav.georgiev@live.com>
2023-11-27 23:14:17 +0200

    feat: video layout with default meta

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,9 @@ class TubeLessApp extends StatelessWidget {
     return MaterialApp(
       title: 'TubeLess',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)
+            .copyWith(background: Colors.black),
       ),
       home: const HomePage(title: 'Home'),
     );
@@ -43,15 +44,14 @@ class _HomePageState extends State<HomePage> {
       var decodedConfig = jsonDecode(config) as Map;
 
       var queryParams = {
-        'part': 'snippet',
+        'part': 'snippet,statistics,contentDetails',
         'maxResults': '50',
-        'q': 'dotnet',
         'key': decodedConfig['apiKey'] as String,
-        'type': 'video'
+        'chart': 'mostPopular',
       };
 
       var address = Uri.https(
-          'content-youtube.googleapis.com', '/youtube/v3/search', queryParams);
+          'content-youtube.googleapis.com', '/youtube/v3/videos', queryParams);
 
       var response = await client.get(address);
       var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
@@ -62,15 +62,160 @@ class _HomePageState extends State<HomePage> {
         var snippet = item['snippet'] as Map;
         var title = snippet['title'] as String;
         var channelTitle = snippet['channelTitle'] as String;
+
+        var publishedAtDate = DateTime.parse(snippet['publishedAt'] as String);
+        var publishedAtDiff = DateTime.now().difference(publishedAtDate);
+        var publishedAt = publishedAtDiff.inDays > 365
+            ? '${(publishedAtDiff.inDays / 365).floor()} years ago'
+            : publishedAtDiff.inDays > 30
+                ? '${(publishedAtDiff.inDays / 30).floor()} months ago'
+                : publishedAtDiff.inDays > 1
+                    ? '${publishedAtDiff.inDays} days ago'
+                    : publishedAtDiff.inDays == 1
+                        ? '1 day ago'
+                        : publishedAtDiff.inHours > 1
+                            ? '${publishedAtDiff.inHours} hours ago'
+                            : publishedAtDiff.inHours == 1
+                                ? '1 hour ago'
+                                : publishedAtDiff.inMinutes > 1
+                                    ? '${publishedAtDiff.inMinutes} minutes ago'
+                                    : publishedAtDiff.inMinutes == 1
+                                        ? '1 minute ago'
+                                        : 'just now';
+
         var thumbnails = snippet['thumbnails'] as Map;
-        var defaultThumbnail = thumbnails['default'] as Map;
+        var defaultThumbnail = thumbnails['maxres'] as Map;
+        if (defaultThumbnail.isEmpty) {
+          defaultThumbnail = thumbnails['high'] as Map;
+        } else if (defaultThumbnail.isEmpty) {
+          defaultThumbnail = thumbnails['medium'] as Map;
+        } else if (defaultThumbnail.isEmpty) {
+          defaultThumbnail = thumbnails['standard'] as Map;
+        } else if (defaultThumbnail.isEmpty) {
+          defaultThumbnail = thumbnails['default'] as Map;
+        }
         var thumbnailUrl = defaultThumbnail['url'] as String;
 
-        list.add(ListTile(
-          leading: Image.network(thumbnailUrl),
-          title: Text(title),
-          subtitle: Text(channelTitle),
-        ));
+        var statistics = item['statistics'] as Map;
+        var viewCountRaw = int.parse(statistics['viewCount'] as String);
+        var viewCount = viewCountRaw > 1000000
+            ? '${(viewCountRaw / 1000000).toStringAsFixed(1)}M'
+            : viewCountRaw > 1000
+                ? '${(viewCountRaw / 1000).toStringAsFixed(1)}K'
+                : viewCountRaw.toString();
+
+        var contentDetails = item['contentDetails'] as Map;
+        var durationRaw = contentDetails['duration'] as String;
+        var duration = durationRaw
+            .replaceAll('PT', '')
+            .replaceAll('H', ':')
+            .replaceAll('M', ':')
+            .replaceAll('S', '');
+
+        var durationSplit = duration.split(':');
+        if (durationSplit.length == 3) {
+          var durationHour = durationSplit[0];
+          if (durationHour.length == 1) {
+            durationHour = '0$durationHour';
+          }
+          var durationMin = durationSplit[1];
+          if (durationHour.isNotEmpty && durationMin.length == 1) {
+            durationMin = '0$durationMin';
+          } else if (durationHour.isEmpty && durationMin.isEmpty) {
+            durationMin = '00';
+          }
+          var durationSec = durationSplit[2];
+          if (durationSec.length == 1) {
+            durationSec = '0$durationSec';
+          }
+          duration = '$durationHour:$durationMin:$durationSec';
+        } else if (durationSplit.length == 2) {
+          var durationMin = durationSplit[0];
+          var durationSec = durationSplit[1];
+          if (durationSec.length == 1) {
+            durationSec = '0$durationSec';
+          } else if (durationSec.isEmpty) {
+            durationSec = '00';
+          }
+          duration = '$durationMin:$durationSec';
+        } else if (durationSplit.length == 1) {
+          var durationSec = durationSplit[0];
+          if (durationSec.length == 1) {
+            durationSec = '0$durationSec';
+          }
+          duration = '00:$durationSec';
+        }
+
+        list.add(
+          Container(
+            width: 380,
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Stack(
+                  children: [
+                    Image.network(
+                      thumbnailUrl,
+                      fit: BoxFit.fill,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) {
+                          return child;
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      },
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      right: 10,
+                      child: Container(
+                        padding: const EdgeInsets.all(4),
+                        color: Colors.black,
+                        child: Text(
+                          duration,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.all(6),
+                  color: Colors.black,
+                  constraints: const BoxConstraints(maxHeight: 70),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                            color: Colors.white,
+                          )),
+                      Text('@$channelTitle • $viewCount views • $publishedAt',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white70,
+                          )),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
       }
 
       if (kDebugMode) {
@@ -78,6 +223,11 @@ class _HomePageState extends State<HomePage> {
       }
 
       return list;
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      rethrow;
     } finally {
       client.close();
     }
@@ -91,6 +241,8 @@ class _HomePageState extends State<HomePage> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return ListView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(16),
               children: snapshot.data!,
             );
           } else if (snapshot.hasError) {
